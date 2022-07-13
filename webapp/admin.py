ab624f944cf2b20df722c5972a1e0b8706e07267
Aziz_Is <Aziz10877@gmail.com>
2022-07-13 17:12:51 +0600

    fixtures has been created

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Tracker
+from .models import Tracker, Status, Type
 
 # Register your models here.
 admin.site.register(Tracker)
+admin.site.register(Status)
+admin.site.register(Type)
+
--- webapp/models.py
+++ webapp/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Status(models.Model):
+    status_choices = [('new', 'новый'), ('in progress', 'в процессе'), ('done', 'выполнено')]
+    status = models.CharField(max_length=20, choices=status_choices, default='new')
+    def __str__(self):
+        return f'{self.status}'
+
+
+class Type(models.Model):
+    type_choices = [('task', 'задача'), ('bug', 'ошибка'), ('enhancement', 'улучшение')]
+    type = models.CharField(max_length=20, choices=type_choices, default='task')
+    def __str__(self):
+        return f'{self.type}'
+
 
 class Tracker(models.Model):
     summary = models.CharField(max_length=200, null=False, blank=False, verbose_name='zagolovok')
     description = models.TextField(max_length=1000, null=False, blank=False, verbose_name='opisanie')
+    status = models.ForeignKey(Status, related_name='tracker_status', on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, related_name='tracker_type', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Vremy sozdaniy')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='vremy izmeneniy')
 
@@ -14,10 +30,5 @@ class Tracker(models.Model):
     class Meta:
         pass
 
-    class Status:
-        pass
-
-    class Type:
-        pass
 
 
