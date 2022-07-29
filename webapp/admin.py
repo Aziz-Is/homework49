c1efc764d5c327d45255a7934675fbb4ba014340
Aziz-Is <107648849+Aziz-Is@users.noreply.github.com>
2022-07-29 16:47:58 +0600

    Delete webapp directory

--- webapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tracker, Status, Type
-
-# Register your models here.
-admin.site.register(Tracker)
-admin.site.register(Status)
-admin.site.register(Type)
-
